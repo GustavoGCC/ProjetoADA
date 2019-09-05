b131ffd9cc22ae36f18b6d6b60a9c71990572e2a
deboraleda <debora.souza@ccc.ufcg.edu.br>
2019-09-04 22:48:37 -0300

    abre diretorio e ler arquivo

--- alunos/folderReader.cpp
+++ alunos/folderReader.cpp
@@ -5,49 +5,61 @@
 #include <stdio.h>
 #include <iostream>
 #include <string>
+#include <fstream>
 
-#include <typeinfo>
 using namespace std;
 
-void explore(char *dir_name) {
-	DIR *dir; //pointer to an open director
-	struct dirent *entry;	//stuff in the directory
-	struct stat info;	//information about each entry
+//funcoes
+void readFile(string path);
 
-	//1 open
-	dir = opendir(dir_name);
-	if (!dir) {
-		cout << "Directory was not found\n" << endl;
-		return;
+void openDirectory(char * dir_name){
+     DIR *dir; //pointer to open directory
+     struct dirent *entry; //stuff inside the direct
+     struct stat info; //information about each entry
+
+     //1 open
+     dir = opendir(dir_name);
+     if (!dir) {
+      cout << "Directory was not found";
+      return;
+    }
+
+     //2read
+     while ((entry = readdir(dir)) != NULL)
+     {
+          if (entry->d_name[0] != '.')
+          {
+           string path = string(dir_name) + '/' + string(entry->d_name);
+           readFile(path);
+           stat(path.c_str(), &info);
+           if(S_ISDIR(info.st_mode)){
+                openDirectory((char*)path.c_str());
+           }
+          }
+     }
+
+     //3close
+     closedir(dir);
+}
+
+void readFile(string path){
+    fstream myFile;
+	myFile.open(path.c_str());
+
+	while(myFile.good()){
+		string cell;
+		std::getline(myFile, cell, '\n');
+		cout << cell << endl;
 	}
-
-	//2 read
-	while ((entry = readdir(dir)) != NULL) {
-		if (entry->d_name[0] != '.') {
-			
-			string path = string(dir_name) + "/" + string(entry->d_name);
-			cout << "Entry = " << path << endl;
-			stat(path.c_str(),&info);
-			if(S_ISDIR(info.st_mode)) {
-				explore((char*)path.c_str());
-			}
-		}
-	}
-
-	//3 close
-	closedir(dir);
 }
 
 
 int main() {
-
-    string a;
-    std::cin >> a;
-    char ar[a.size()];
-    for(int i = 0 ; i < a.size(); i++){
-        ar[i] = (char) a[i];
-    }
-
-	explore(ar); 
-	return 0;
+ string diretorio = "";
+ cout << "digite o diretorio que contem os historicos dos alunos" << endl;
+ cin >> diretorio;
+ //"C:/Users/Debora/Documents/entradaDados";
+ openDirectory( (char*)diretorio.c_str());
+ return 0;
 }
+
