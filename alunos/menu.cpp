99969e896f4fe7a1cf80a83ebb5d052882f093c8
deboraleda <debora.souza@ccc.ufcg.edu.br>
2019-09-12 01:52:03 -0300

    add opcao voltar em cada pagina

--- alunos/menu.cpp
+++ alunos/menu.cpp
@@ -8,15 +8,17 @@
 using namespace std;
 
 //funcoes
-int paginaInicial();
+void paginaInicial();
 string centralizar(string str);
 void menu();
 void exibirFuncoesAlunos();
+void exibirFuncoesDisciplina();
 void importarDados();
 void alunos();
 void disciplina();
+void informacao();
 
-int paginaInicial(){
+void paginaInicial(){
     system("clear||cls");
     cout << "\n\n";
     cout << centralizar("    |     '||''|.       |     ") + "\n" + centralizar("   |||     ||   ||     |||    ") + "\n" +
@@ -26,22 +28,17 @@ int paginaInicial(){
     cout << "\n\n";
     cout << "\t\t\t\t     1.menu\t\t2.info\n\n";
     int opcao;
-    cout << "\t\t\t\t   opcao > ";
-    cin >> opcao;
-    return opcao;
-}
+    do{
+        cout << "\t\t\t\t   opcao > ";
+        cin >> opcao;
+    }while(opcao != 1 && opcao != 2);
+    if(opcao == 1){
+        menu();
+    }else{
+        informacao();
+    }
 
-void exibirFuncoesAlunos(){
-    system("clear||cls");
-    cout << "\n\n" << centralizar("Funcionalidades")
-    << "\n\n" << centralizar("1.agrupar alunos de acordo com o CRA passado como parametro")
-    << "\n" << centralizar("2.exibir o desempenho dos alunos nos pre requisitos")
-    << "\n" << centralizar("3.exibir o desempenho dos alunos nas disciplinas desejaveis")
-    << "\n" << centralizar("4.exibir alunos que possuem ao menos n reprovacoes no curriculo")
-    << "\n" << centralizar("5.exibir alunos que reprovaram a disciplina");
-    int opcao;
-    cout << "\n\n\t\t\t\t   opcao > ";
-    cin >> opcao;
+
 }
 
 void importarDados(){
@@ -62,16 +59,60 @@ void importarDados(){
 void alunos(){
     system("clear||cls");
     cout << "\n\n" << centralizar("voce deseja")
-    << "\n\n\t\t\t\t1.importar dados   2.exibir funcoes     3.voltar\n\n" << "\t\t\t\t   opcao > ";
+    << "\n\n\t\t\t1.importar dados   2.exibir funcoes     3.voltar\n\n";
     int opcao;
-    cin >> opcao;
-
+    do{
+        cout << "\t\t\t\t   opcao > ";
+        cin >> opcao;
+    }while(opcao != 1 && opcao != 2 && opcao != 3);
     if(opcao == 1){ //importar dados
         importarDados();
     }else if(opcao == 2){ //exibir funcionalidades de aluno
         exibirFuncoesAlunos();
+    }else if(opcao == 3){
+        menu();
     }
 
+}
+
+void disciplina(){
+    system("clear||cls");
+    cout << "\n\n" << centralizar("voce deseja")
+    << "\n\n\t\t\t1.importar dados   2.exibir funcoes     3.voltar\n\n";
+    int opcao;
+    do{
+        cout << "\t\t\t\t   opcao > ";
+        cin >> opcao;
+    }while(opcao != 1 && opcao != 2 && opcao != 3);
+    if(opcao == 1){ //importar dados
+        //importarDados(); // comentei pois nao sei se funciona pq a parte de importar dados de disciplina eh do L.L
+    }else if(opcao == 2){ //exibir funcionalidades de aluno
+        exibirFuncoesDisciplina();
+    }else if(opcao == 3){
+        menu();
+    }
+}
+
+void exibirFuncoesAlunos(){
+    system("clear||cls");
+    cout << "\n\n" << centralizar("Funcionalidades")
+    << "\n\n" << centralizar("1.agrupar alunos de acordo com o CRA passado como parametro")
+    << "\n" << centralizar("2.exibir o desempenho dos alunos nos pre requisitos")
+    << "\n" << centralizar("3.exibir o desempenho dos alunos nas disciplinas desejaveis")
+    << "\n" << centralizar("4.exibir alunos que possuem ao menos n reprovacoes no curriculo")
+    << "\n" << centralizar("5.exibir alunos que reprovaram a disciplina");
+    int opcao;
+    cout << "\n\n\t\t\t\t   opcao > ";
+    cin >> opcao;
+    switch(opcao){
+    case 1:
+    case 2:
+    case 3:
+    case 4:
+    case 5:
+    default:
+        alunos(); // se digitar algum valor que nao esta entre 1 e 5, volta a pagina anterior
+    }
 }
 
 void exibirFuncoesDisciplina(){
@@ -84,18 +125,13 @@ void exibirFuncoesDisciplina(){
     int opcao;
     cout << "\n\n\t\t\t\t   opcao > ";
     cin >> opcao;
-}
-
-void disciplina(){
-    system("clear||cls");
-    cout << "\n\n" << centralizar("voce deseja")
-    << "\n\n\t\t\t\t1.importar dados   2.exibir funcoes     3.voltar\n\n" << "\t\t\t\t   opcao > ";
-    int opcao;
-    cin >> opcao;
-    if(opcao == 1){ //importar dados
-        //importarDados(); // comentei pois nao sei se funciona pq a parte de importar dados de disciplina eh do L.L
-    }else if(opcao == 2){ //exibir funcionalidades de aluno
-        exibirFuncoesDisciplina();
+    switch(opcao){
+    case 1:
+    case 2:
+    case 3:
+    case 4:
+    default:
+        disciplina(); // se digitar algum valor que nao esta entre 1 e 5, volta a pagina anterior
     }
 }
 
@@ -107,41 +143,42 @@ string centralizar(string str) {
 }
 
 void menu(){
-    int opcao = paginaInicial();
-
-    if(opcao==1){ //opcao menu
-        system("clear||cls");
-        cout << "\n\n" << centralizar("voce deseja conhecer mais sobre");
-        cout << "\n\n\t\t\t\t1.alunos    2.disciplina    3.voltar\n\n";
+    //opcao menu
+    system("clear||cls");
+    cout << "\n\n" << centralizar("voce deseja conhecer mais sobre");
+    cout << "\n\n\t\t\t\t1.alunos    2.disciplina    3.voltar\n\n";
+    int opcao;
+    do{
         cout << "\t\t\t\t   opcao > ";
         cin >> opcao;
-        if(opcao == 1){
-            alunos();
-        }else if(opcao == 2){
-            disciplina();
-        }else{
-            menu();
-        }
-    }else{ // opcao informação
-        system("clear||cls");
-        cout << "\n\n " << centralizar("venha conhecer seus alunos ") << "\n\n";
-        cout << centralizar("ADA eh uma aplicacao visa auxiliar o professor em sala de aula de forma ")
-             << "\n" << centralizar("que seja repassado para ele uma analise da turma sobre a qual ele ira ")
-             << "\n" << centralizar("ministrar. O perfil da turma sera traçado baseado no historico dos alunos e ")
-             << "\n" << centralizar("no historico da disciplina.");
-        cout << "\n\n " << centralizar("1.voltar") << "\n\n";
-        int op;
-        do{
-          cout << "\t\t\t\t   opcao > ";
-          cin >> op;
-        }while(op != 1);
-        menu();
-
+    }while(opcao != 1 && opcao != 2 && opcao != 3);
+    if(opcao == 1){
+        alunos();
+    }else if(opcao == 2){
+        disciplina();
+    }else if(opcao == 3){
+        paginaInicial();
     }
 
 }
 
+void informacao(){
+    system("clear||cls");
+    cout << "\n\n " << centralizar("venha conhecer seus alunos ") << "\n\n";
+    cout << centralizar("ADA eh uma aplicacao que visa auxiliar o professor em sala de aula de forma ")
+         << "\n" << centralizar("que seja repassado para ele uma analise da turma sobre a qual ele ira ")
+         << "\n" << centralizar("ministrar. O perfil da turma sera traçado baseado no historico dos alunos e ")
+         << "\n" << centralizar("no historico da disciplina.");
+    cout << "\n\n " << centralizar("1.voltar") << "\n\n";
+    int op;
+    do{
+      cout << "\t\t\t\t   opcao > ";
+      cin >> op;
+    }while(op != 1);
+    paginaInicial();
+}
+
 int main (){
-    menu();
+    paginaInicial();
 return 0;
 }
