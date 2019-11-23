425204a80241497768a51147ed329d94d240a416
deboraleda <debora.souza@ccc.ufcg.edu.br>
2019-11-23 14:00:35 -0300

    Alterações em Ler arquivo

--- AdaProlog/Lerarquivo.pl
+++ AdaProlog/Lerarquivo.pl
@@ -1,17 +1,30 @@
 :- use_module(library(csv)).
 
 
-lerCsv(File):- 
-csv_read_file("./arquivo.csv", File).
+lerCsv(Arquivo, File) :- 
+csv_read_file(Arquivo, File).
 
-varrerDiretorio(Diretorio, Files):-
-directory_files(Diretorio, Files).
+abrirDiretorio(Diretorio,Files, Res, Lists):-
+directory_files(Diretorio, Files),
+subtract(Files, ['.', '..'], Res),
+varrerDiretorio(Res, Lists).
 
-lerCsvRowList(Lists, L):-
-csv_read_file("./arquivo.csv", Rows, []),
-rows_to_lists(Rows, Lists),
-append([["10", "psoft"], ["10", "java"]], Lists, L).
+
+varrerDiretorio([X|[]], Lists):- lerArquivo(X, Lists).
+varrerDiretorio([X|L1], Lists):-
+lerArquivo(X, Lists),
+varrerDiretorio(L1, Lists).
+
+lerArquivo(Arquivo, Lists):-
+atom_concat("./historicosDosAlunos/",Arquivo, Res),
+csv_read_file(Res, Rows, []),
+rows_to_lists(Rows, Lists).
+
+lerCsvRowList(Lists):-
+ csv_read_file("./arquivo.csv", Rows, []),
+ rows_to_lists(Rows, Lists).
 
 rows_to_lists(Rows, Lists):- maplist(row_to_list, Rows, Lists).
+
 row_to_list(Row, List):-
-Row =.. [row|List].
+ Row =.. [row|List].
